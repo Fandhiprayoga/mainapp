6485c66630e7c9746bab1b058f35b0c4d70b2216
fandhiprayoga <fduga2@gmail.com>
2018-10-15 10:00:26 +0700

    tambah db terbaru

--- db/mainapp (1).sql
+++ db/mainapp (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 10, 2018 at 04:15 AM
+-- Generation Time: Oct 15, 2018 at 04:59 AM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.11
 
@@ -52,7 +52,10 @@ INSERT INTO `group_priv` (`id`, `id_group_user`, `id_submenu`, `is_priv`) VALUES
 (10, 2, 6, 1),
 (11, 1, 5, 1),
 (12, 1, 7, 1),
-(13, 2, 3, 0);
+(13, 2, 3, 0),
+(14, 2, 7, 0),
+(15, 1, 12, 1),
+(16, 1, 14, 1);
 
 -- --------------------------------------------------------
 
@@ -77,6 +80,28 @@ INSERT INTO `group_user` (`id_group_user`, `nama_group_user`, `keterangan_group_
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `instansi`
+--
+
+CREATE TABLE `instansi` (
+  `id` int(11) NOT NULL,
+  `nama_instansi` text,
+  `alamat_instansi` text,
+  `kepala_instansi` text,
+  `nik_instansi` text,
+  `logo_instansi` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `instansi`
+--
+
+INSERT INTO `instansi` (`id`, `nama_instansi`, `alamat_instansi`, `kepala_instansi`, `nik_instansi`, `logo_instansi`) VALUES
+(1, 'PONPES AN-NAJAH2', 'aaaaaaaaaaaaaaaaaa2', 'fandhi dhuga prayoga2', '111111111111111111111111asasas', 'fae1a45fcef44d1a625aab5b5ee89226.png');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `menu`
 --
 
@@ -95,9 +120,10 @@ CREATE TABLE `menu` (
 INSERT INTO `menu` (`id`, `id_modul`, `nama_menu`, `icon_menu`, `order_menu`) VALUES
 (1, 5, 'MODUL', 'fas fa-boxes', 1),
 (2, 5, 'USER', 'fab fa-centercode', 2),
-(3, 5, 'MASTER', 'fas fa-arrow-circle-right', 3),
+(3, 5, 'MASTER', 'fas fa-arrow-circle-right', 4),
 (4, 5, 'TEST 2', 'fas fa-chart-line', 4),
-(5, 6, 'Lalalala', 'fab fa-bitcoin', 1);
+(5, 6, 'Lalalala', 'fab fa-bitcoin', 1),
+(6, 5, 'INSTANSI', 'far fa-building', 3);
 
 -- --------------------------------------------------------
 
@@ -128,6 +154,33 @@ INSERT INTO `modul` (`id`, `nama_modul`, `icon`, `link_modul`, `order_modul`, `s
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `mst_staff`
+--
+
+CREATE TABLE `mst_staff` (
+  `id_staff` varchar(50) NOT NULL,
+  `nama_staff` text,
+  `jk_staff` varchar(10) DEFAULT NULL,
+  `t_staff` varchar(50) DEFAULT NULL,
+  `tl_staff` date DEFAULT NULL,
+  `jabatan_staff` text,
+  `foto_staff` text,
+  `alamat_staff` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mst_staff`
+--
+
+INSERT INTO `mst_staff` (`id_staff`, `nama_staff`, `jk_staff`, `t_staff`, `tl_staff`, `jabatan_staff`, `foto_staff`, `alamat_staff`) VALUES
+('A201810122', 'ANI WIJAYA NDANU', 'PEREMPUAN', 'BANYUMAS', '2010-12-18', 'ADMIN SOSMED', NULL, 'jl kenangan no 1, purwokerto, banyumas'),
+('A201810131', 'anooo', 'LAKI-LAKI', 'bannyumas', '2018-10-26', 'ADMIN SOSMED', NULL, 'purwokerto'),
+('H201810132', 'ano nooo', 'PEREMPUAN', 'CILACAP', '2018-10-25', 'HRD', '3ded98dfc70f8956b61fc083877bda21.jpg', 'AAAAAAA'),
+('U201810121', 'FANDHI DHUGA PRAYOGA', 'LAKI-LAKI', 'BANYUMAS', '1994-01-07', 'TEKNOLOGI INFORMASI2', '8022e29ee409e09d0d28b0ca1a37f1c0.jpg', 'Cilongok');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `submenu`
 --
 
@@ -150,11 +203,13 @@ INSERT INTO `submenu` (`id`, `id_menu`, `nama_submenu`, `link_submenu`, `order_s
 (4, 2, 'User', 'index.php/modul_admin/user', 1),
 (5, 2, 'Group User', 'index.php/modul_admin/group_user', 2),
 (6, 2, 'Group Privilage', 'index.php/modul_admin/group_priv', 4),
-(7, 3, 'Master Lain-lain2', '#', 1),
+(7, 3, 'Master Data Santri', 'index.php/modul_admin/mst_santri', 2),
 (8, 4, 'aaaaaaaaaaaa', '#', 1),
-(9, 5, 'aaaaaaaaaaaaaaa', '#', 1),
+(9, 5, 'bbbbbbbbbbbbb', '#', 1),
 (10, 5, 'bbbbbbbbbbbbbbb', '#', 1),
-(11, 5, 'cccccccccccc', '#', 1);
+(11, 5, 'cccccccccccc', '#', 1),
+(12, 6, 'Setting Instansi', 'index.php/modul_admin/instansi', 1),
+(14, 3, 'Master Data Staff', 'index.php/modul_admin/mst_staff', 1);
 
 -- --------------------------------------------------------
 
@@ -164,8 +219,7 @@ INSERT INTO `submenu` (`id`, `id_menu`, `nama_submenu`, `link_submenu`, `order_s
 
 CREATE TABLE `user` (
   `id_user` varchar(8) NOT NULL,
-  `nama_user` varchar(50) DEFAULT NULL,
-  `bagian_user` varchar(50) DEFAULT NULL,
+  `id_staff` varchar(50) DEFAULT NULL,
   `group_user` int(11) DEFAULT NULL,
   `pass_user` varchar(8) DEFAULT NULL,
   `status_user` varchar(8) DEFAULT NULL
@@ -175,10 +229,9 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id_user`, `nama_user`, `bagian_user`, `group_user`, `pass_user`, `status_user`) VALUES
-('admin', 'Fandhi Dhuga Prayoga', 'TI', 1, 'admin', 'Aktif'),
-('admin2', 'admin2', 'fdsfdsf', 2, 'admin2', 'Aktif'),
-('admin4', 'admin4', 'aaaaaaaaa', 1, 'aaaaaa', 'Aktif');
+INSERT INTO `user` (`id_user`, `id_staff`, `group_user`, `pass_user`, `status_user`) VALUES
+('admin', 'U201810121', 1, 'admin', 'Aktif'),
+('admin2', 'H201810132', 2, 'admin2', 'Aktif');
 
 --
 -- Indexes for dumped tables
@@ -199,6 +252,12 @@ ALTER TABLE `group_user`
   ADD PRIMARY KEY (`id_group_user`);
 
 --
+-- Indexes for table `instansi`
+--
+ALTER TABLE `instansi`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `menu`
 --
 ALTER TABLE `menu`
@@ -212,6 +271,12 @@ ALTER TABLE `modul`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `mst_staff`
+--
+ALTER TABLE `mst_staff`
+  ADD PRIMARY KEY (`id_staff`);
+
+--
 -- Indexes for table `submenu`
 --
 ALTER TABLE `submenu`
@@ -223,7 +288,8 @@ ALTER TABLE `submenu`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id_user`),
-  ADD KEY `groupuser_user` (`group_user`);
+  ADD KEY `groupuser_user` (`group_user`),
+  ADD KEY `staff_user` (`id_staff`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -233,7 +299,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `group_priv`
 --
 ALTER TABLE `group_priv`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `group_user`
@@ -242,10 +308,16 @@ ALTER TABLE `group_user`
   MODIFY `id_group_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT for table `instansi`
+--
+ALTER TABLE `instansi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `menu`
 --
 ALTER TABLE `menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `modul`
@@ -257,7 +329,7 @@ ALTER TABLE `modul`
 -- AUTO_INCREMENT for table `submenu`
 --
 ALTER TABLE `submenu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- Constraints for dumped tables
@@ -286,7 +358,8 @@ ALTER TABLE `submenu`
 -- Constraints for table `user`
 --
 ALTER TABLE `user`
-  ADD CONSTRAINT `groupuser_user` FOREIGN KEY (`group_user`) REFERENCES `group_user` (`id_group_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `groupuser_user` FOREIGN KEY (`group_user`) REFERENCES `group_user` (`id_group_user`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `staff_user` FOREIGN KEY (`id_staff`) REFERENCES `mst_staff` (`id_staff`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
